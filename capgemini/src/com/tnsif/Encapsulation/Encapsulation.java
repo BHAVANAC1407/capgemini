7d0d2ec625b2008e2fa467f644dbc6cd3383fc15
Bhavana <Bhavana@DESKTOP-LEE6PV6>
2024-06-25 19:18:15 +0530

    firstcommit

---
+++ capgemini/src/com/tnsif/Encapsulation/Encapsulation.java
@@ -0,0 +1,5 @@
+package com.tnsif.Encapsulation;
+
+public class Encapsulation {
+
+}
---
+++ capgemini/src/com/tnsif/Encapsulation/EncasulationDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.Encapsulation;
+
+public class EncasulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				Encapsulation ob= new Encapsulation();// object created
+				ob.name="suma";
+				ob.age=33;
+				ob.serialNum=22;
+				ob.show();
+				
+
+	}
+
+}
